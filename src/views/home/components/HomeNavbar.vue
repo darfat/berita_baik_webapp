8e7f048e1a6f562dcf0be868e814a126f4d7c52f
kashmirx <kashmirx@gmail.com>
2018-02-19 06:45:58 +0700

    *delete

--- src/views/home/components/HomeNavbar.vue
+++ src/views/home/components/HomeNavbar.vue
@@ -1,35 +1,13 @@
 <template>
-  <div class="menu-wrapper">
-    <el-menu
-    :default-active="activeIndex"
-    class="container el-menu-nav"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#F5DF00"
-    text-color="#0A2F88"
-    active-text-color="#0A2F88">
-      <el-menu-item index="1">Home</el-menu-item>
-      <el-menu-item index="2"><a href="">Indonesia Baik</a></el-menu-item>
-      <el-menu-item index="3"><a href="">Indonesia Membangun</a></el-menu-item>
-      <el-menu-item index="4"><a href="">Indonesia Bangga</a></el-menu-item>
-      <el-menu-item index="5"><a href="">Melancong</a></el-menu-item>
-      <el-menu-item index="6"><a href="">Teknologi</a></el-menu-item>
-      <el-menu-item index="7"><a href="">Panggung</a></el-menu-item>
-      <el-menu-item index="8"><a href="">Citra</a></el-menu-item>
-    </el-menu>
-  </div>
+  
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      activeIndex: '1',
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+      x: '1'
     }
   }
 }
@@ -37,13 +15,23 @@ console.log('home navbar')
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-@import "src/styles/home.scss";
+@import "src/styles/variables.scss";
+.menu-logo{
+  height: 40px;
+  width: 40px;
+  background-color: rgba(255,255,255  ,0.6 );
+  border-radius: 50%;
+}
+.menu-logo-img{
+  height: 30px;
+  margin: 5px
+}
 .menu-wrapper {
-  background-color: $bb-yellow;
-  font-weight: 700;
-
+  background-color: $bb-yellow;  
+  padding: 0;
+  margin: 0;
   .el-menu-nav{
-    
+    color: #FFF;
   }
 }
 </style>
