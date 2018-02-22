086affa13ed5bc0338e16fdc50800216e95cae1e
Darmawan Fatriananda <darmawan.fatria@gmail.com>
2018-02-22 18:46:59 +0000

    style article detail

--- src/views/portal/article/index.vue
+++ src/views/portal/article/index.vue
@@ -5,7 +5,7 @@
           <el-col :span="2"><div class="grid-content"></div></el-col>
           <el-col :span="16">
             <div class="grid-content title-container">
-                <span class="title"> Indonesia Baik </span> 
+                <span class="main-article-title"> Indonesia Baik </span> 
             </div>
           </el-col>
       </el-row>
@@ -24,61 +24,80 @@
                   </div>
                 </el-col>
               </el-row>
-              <el-row :gutter="20">
-                  <el-col :span="2"><div>love</div></el-col>
-                  <el-col :span="2"><div>like</div></el-col>
-              </el-row>
-              <el-row :gutter="20" class="title">
-                <el-col >
-                  <div > 
-                    {{ mainArticle.title }}
-                  </div>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20">
-                  <el-col :span="1"><article-separator></article-separator></el-col>
-              </el-row>
-              <el-row :gutter="20">
-                  <el-col class="footer">
-                    <div>
-                      {{ mainArticle.reporter.name }} | {{ mainArticle.publish_date_counter }}
-                    </div>
-                  </el-col>
-              </el-row>
-              <el-row :gutter="20">
+              <el-row class="content">
+                <el-row :gutter="20" class="title">
                   <el-col >
-                    <div v-html="mainArticle.content">
-                      
+                    <div > 
+                      {{ mainArticle.title }}
                     </div>
                   </el-col>
+                </el-row>
+                <el-row :gutter="20">
+                    <el-col :span="1"><article-separator></article-separator></el-col>
+                </el-row>
+                <el-row :gutter="20">
+                    <el-col class="footer">
+                      <div>
+                        {{ mainArticle.reporter.name }} | {{ mainArticle.publish_date_counter }}
+                      </div>
+                    </el-col>
+                </el-row>
+                <el-row :gutter="20">
+                    <el-col >
+                      <div v-html="mainArticle.content">
+                        
+                      </div>
+                    </el-col>
+                </el-row>
+                <el-row >
+                  <el-col>
+                    <div class="h-divider m-tb-10"></div>
+                  </el-col>
+                </el-row>
+                <el-row :gutter="20" class="m-t-10 article-option">
+                    <el-col :span="1">
+                      <span  > 
+                        Editorial <br/> Team
+                      </span>
+                    </el-col>
+                    <el-col :span="2">
+                      <img :src="mainArticle.reporter.image" class="img-circle img-mini v-align-middle"/>
+                      <span> {{ mainArticle.reporter.name }} </span>
+                    </el-col>
+                    <el-col :span="13" class="p-tb-5">
+                      <div class="follow-user">
+                        <v-icon name="user" base-class="icon-20 v-align-middle"></v-icon>
+                        <span  > 
+                          Follow
+                        </span>
+                      </div>
+                    </el-col>
+                    <el-col :span="7" class="align-right" >
+                      <span> <v-icon name="heart" base-class="icon-20"></v-icon> </span>
+                      <span> <v-icon name="facebook" base-class="icon-20"></v-icon> </span>
+                      <span> <v-icon name="mail" base-class="icon-20"></v-icon> </span>
+                      <span> <v-icon name="send" base-class="icon-20"></v-icon> </span>
+                      <span> <v-icon name="twitter" base-class="icon-20"></v-icon> </span>
+                    </el-col>
+                    <el-col :span="1">                    
+                      <span> <v-icon name="more-horizontal" base-class="icon-20"></v-icon> </span>
+                  </el-col>
+                </el-row>
               </el-row>
-              <el-row >
-                <el-col>
-                  <div class="h-divider m-tb-10"></div>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20">
-                  <el-col :span="16"><div>editorial</div></el-col>
-                  <el-col :span="2"><div>love</div></el-col>
-                  <el-col :span="4"><div>share</div></el-col>
-                  <el-col :span="2"><div>report</div></el-col>
-
-              </el-row>
-              <el-row :gutter="20" >
-                <el-col>
-                  <div class="write-comments">
-                      comments component
-                  </div>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20" >
-                <el-col>
-                  <div class="paging">
-                      paging component
-                  </div>
-                </el-col>
-              </el-row>
+              <el-row :gutter="20" class="m-t-10" >
+            <el-col>
+              <comment-box></comment-box>
+            </el-col>
+          </el-row>
         </div>
+        <div>
+          <el-row :gutter="20">
+            <el-col>
+              <article-nav></article-nav>
+            </el-col>
+          </el-row>
+        </div>
+       
       </el-col>
       <el-col :span="2"><div class="grid-content"></div></el-col>
     </el-row>
@@ -96,51 +115,7 @@
 
       <el-col :span="14" class="content">
         <div class="grid-content a-content">
-            <el-row>
-              <div class="section-title"> BACA LAINNYA</div>
-            </el-row>
-            <el-row>
-              <el-col :span="10" v-for="(article, index) in articles" :key="article.id" class="news-col">
-                <el-card :body-style="{ padding: '0px' }" class="news-card">
-                  <div>
-                      <div class="mini-thumbnail">
-                        <img :src="article.main_image" class="card-image" />
-                        <div class="editorial-type-img">
-                            <p style="font-size:10px;">{{ article.editorial }}</p>
-                        </div>
-                      </div>
-                  </div>
-                  <div style="padding: 14px;">
-                    <div class="bottom clearfix">
-                      <el-row >
-                        <el-col :span="2"><div>love</div></el-col>
-                        <el-col :span="2"><div>like</div></el-col>
-                    </el-row>
-                    </div>
-                    <el-row >
-                      <div>
-                        <span>{{ article.title}}</span>
-                      </div>
-                    </el-row>
-                    <el-row >
-                        <el-col :span="2"><article-separator></article-separator></el-col>
-                    </el-row>
-                    <el-row >
-                        <el-col :span="4">
-                          <div>
-                            {{ article.reporter.name }}
-                          </div>
-                        </el-col>
-                        <el-col :span="10">
-                          <div>
-                            {{ article.publish_date_counter }}
-                          </div>
-                        </el-col>
-                    </el-row>
-                  </div>
-                </el-card>
-              </el-col>
-            </el-row>
+            <articles-card></articles-card>
             <el-row >
               <el-col>
               </el-col>
@@ -149,49 +124,19 @@
       </el-col>
       <el-col :span="6" class="side-content">
         <div class="grid-content a-side">
-          <el-row>
-              <div class="section-title"> BERITA BAIK TERPOPULAR</div>
-          </el-row>
-          <div class="popular-news">
-            <el-row v-for="(article) in popular_articles" :key="article.id"  >
-              <el-col :span="6">
-                <div>
-                  <img :src="article.main_image" class="card-image"/>
-                </div>
-              </el-col>
-              <el-col :span="18">
-                <div>
-                    <el-row >
-                      <div>
-                        <span>{{ article.title}}</span>
-                      </div>
-                    </el-row>
-                    <el-row >
-                        <el-col :span="2"><article-separator></article-separator></el-col>
-                    </el-row>
-                    <el-row >
-                        <el-col :span="6">
-                          <div>
-                            {{ article.reporter.name }}
-                          </div>
-                        </el-col>
-                        <el-col :span="10">
-                          <div>
-                            {{ article.publish_date_counter }}
-                          </div>
-                        </el-col>
-                    </el-row>
-                </div>
-              </el-col>         
-            </el-row>            
-          </div>
+          <popular-news-side> </popular-news-side>
           <div class="side-separator">
             <span> Buka lebih banyak lagi </span>
           </div>
           <div>Infografis</div>
+          <div class="side-separator">
+            <span> Buka lebih banyak lagi </span>
+          </div>
           <div>iklan</div>
-          <div>Pilihan editor</div>
-
+          <div class="side-separator">
+            <span>  </span>
+          </div>
+          <div>pilihan editor</div>
         </div>
       </el-col>
       
@@ -230,29 +175,34 @@
 
 <script>
 import ArticleSeparator from '@/components/ArticleSeparator'
+import { PopularNewsSide, ArticlesCard, CommentBox, ArticleNav } from '@/views/portal/components'
 
 export default {
   name: 'article-detail',
   components: {
-    ArticleSeparator
+    ArticleSeparator,
+    PopularNewsSide,
+    ArticlesCard,
+    CommentBox,
+    ArticleNav
   },
   data() {
     return {
       mainArticle: {
         id: '1',
-        main_image: 'static/images/news/img.jpg',
+        main_image: 'static/upload/images/3.jpg',
         content: '<p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>',
         title: 'Lorem Ipsum Title',
         editorial: 'Indonesia Baik',
         reporter: {
           id: '1',
           name: 'Boim',
-          role: 'reporter'
+          role: 'reporter',
+          image: 'static/upload/images/profile.jpg'
         },
         publish_date_counter: '2 Jam Yang lalu'
       },
-      articles: [],
-      popular_articles: []
+      articles: []
     }
   },
   created() {
--- src/views/portal/components/index.js
+++ src/views/portal/components/index.js
@@ -1,3 +1,5 @@
 export { default as PopularNewsSide } from './PopularNewsSide'
 export { default as ArticlesCard } from './ArticlesCard'
+export { default as CommentBox } from './CommentBox'
+export { default as ArticleNav } from './ArticleNav'
 
