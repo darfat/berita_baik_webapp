7b39d370dfcfb47084e11ea2bc67e96c7260c86a
kashmirx <kashmirx@gmail.com>
2018-03-01 09:29:11 +0700

    init search

--- src/views/portal/components/SearchHome.vue
+++ src/views/portal/components/SearchHome.vue
@@ -1,20 +1,41 @@
 <template>
-    <div class="search">
-        <div class="container">
-            <div class="search-box">
-                <span class="search-box-label">Pencarian</span>                
-                <el-input placeholder="Pencarian" v-model="input1" class="">
-                <el-button slot="append" icon="el-icon-search"></el-button>
-                </el-input>             
-            </div>
-            <div class="search-result">
+  <div class="search">
+      <div class="container">
+        <div class="search-box">
+          <el-row>
+            <el-col :span="4" class="search-box-label">
+              Pencarian
+            </el-col>
+            <el-col :span="20" class="search-box-input">
+              <el-input placeholder="Pencarian" v-model="input1" class="">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+            </el-col>
+          </el-row>
+          <el-row style="margin-top: 10px;">
+            <el-col :span="4">
+              &nbsp;  
+            </el-col>
+            <el-col :span="14" class="search-box-result">
+              117 Artikel | Halaman 1 dari 6                
+            </el-col>
+            <el-col :span="6" class="search-box-advanced">
+              <a href="#/search-advanced">Opsi Lanjutan +</a>
+            </el-col>
+          </el-row>                         
+        </div>
+        <div class="search-result">
+          <el-row :gutter="20">
+            <el-span :md="8" v-for="n in 9">
+              <div style="display: block; width:100%; height: 100px; background-color: #0f0">{{n}}</div>
+            </el-span>
+          </el-row>
+        </div>
+        <div class="search-pagination">
 
-            </div>
-            <div class="search-pagination">
-
-            </div>
-        </div>    
-    </div>
+        </div>
+      </div>    
+  </div>
 </template>
 
 <script>
@@ -67,21 +88,27 @@ export default {
 <style rel="stylesheet/scss" lang="scss" scoped>
 @import "src/styles/variables.scss";
 .search{
-    padding: 40px 0;
-    background-color: $bb-gray;
-    &-box{      
-      
-      &-label{        
-        width: 10%;
-        color: $bb-blue;
-        font-size: 1.5em;
-        display: inline-block;
-        background-color: aqua;
-      }
-      .el-input{        
-        
-        
-      }
-    }    
+  padding: 40px 0;
+  background-color: $bb-gray;
+  &-box{
+    &-label{
+      color: $bb-blue;
+      font-size: 1.5em;
+      line-height: 1.6em;
+      font-weight: 700;
+      text-align: center;        
+    }
+    &-advanced{
+      color: $bb-blue;
+      text-align: right;
+    }
+  }
+
+  &-result{
+    width: 100%;
+    min-height: 400px;
+    background-color: #fff;
+    margin: 20px 0;
+  }
 }
 </style>
