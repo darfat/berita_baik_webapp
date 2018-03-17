349aff3c4e1eedad3854e047f75e0d5fcb7c886b
darmawan.fatriananda <darmawan.fatriananda@mandrill.com.my>
2018-03-17 23:00:56 +0700

    login

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,8 +3,8 @@
  */
 
 export function isvalidUsername(str) {
-  const valid_map = ['admin', 'editor']
-  return valid_map.indexOf(str.trim()) >= 0
+  // const valid_map = ['admin', 'editor']
+  return true
 }
 
 /* 合法uri*/
