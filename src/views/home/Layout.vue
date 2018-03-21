76b02b15594659db9c355fe729297790e166b5e1
kashmirx <kashmirx@gmail.com>
2018-03-21 12:54:11 +0700

    responsive column

--- src/views/home/Layout.vue
+++ src/views/home/Layout.vue
@@ -1,35 +1,18 @@
 <template>
-  <div class="home">
+  <div class="app-wrapper">
     <home-header></home-header>
     <home-main></home-main>
     <home-footer></home-footer>    
-  </div><!--/.home-->
+  </div>
 </template>
-
 <script>
 import { HomeHeader, HomeMain, HomeFooter } from '@/views/home/components'
-
 export default {
   name: 'HomeLayout',
-  components: {
-    HomeHeader,
-    HomeMain,
-    HomeFooter
-  },
-  computed: {
-  }
+  components: { HomeHeader, HomeMain, HomeFooter }
 }
 </script>
-
 <style rel="stylesheet/scss" lang="scss" scoped>
 @import "src/styles/mixin.scss";
-@import "src/styles/home.scss";
-
-.home {
-  height: 100%;
-  width: 100%;
-  background-color: #fff;
-  margin: 0;
-  padding: 0;
-}
+.app-wrapper { @include clearfix; position: relative; height: 100%; width: 100%; }
 </style>
