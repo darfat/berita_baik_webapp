01e18bab244a093cf5ca788acd791f342e268aad
kashmirx <kashmirx@gmail.com>
2018-03-21 12:55:28 +0700

    color text

--- src/views/home/components/HeadlineSlider.vue
+++ src/views/home/components/HeadlineSlider.vue
@@ -1,22 +1,24 @@
 <template>
+<div>
   <el-carousel indicator-position="outside" height="560px" v-loading="loading.headlines">
     <el-carousel-item v-for="item in headlines" :key="item.id">
       <div class="item-wrapper-hl">
         <img :src="item.article.main_image" class="image-hl">
-        <div class="section-hl">{{item.editorial.name}}</div>        
+        <h3>{{item.editorial.name}}</h3>
       </div>
-      <div class="bottom-caption">
-      <span> <bb-love></bb-love> </span>
-      <span> <v-icon name="share-2" base-class="icon-20"></v-icon> </span>
+      <div class="title">
+      <span><bb-love></bb-love></span>
+      <span><v-icon name="share-2" base-class="icon-20"></v-icon></span>
       <h2>{{item.article.title}}</h2>
       <p>{{item.article.teaser}}</p>
       <div class="separator">&nbsp;&nbsp;&nbsp;&nbsp;</div>
-      <div>
+      <div class="author">
         {{ item.article.reporter_name}} | <timeago :since="item.article.publish_date"></timeago>
       </div>
       </div>
     </el-carousel-item>
   </el-carousel>
+</div>
 </template>
 
 <script>
@@ -58,46 +60,50 @@ export default {
 }
 </script>
 
-<style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "src/styles/variables.scss";
 .el-carousel{
   z-index: 1;
 }
 .el-carousel__item{
-  background-color: #FFF;  
+  background-color: #FFF;
+  height: auto;
 }
 .image-hl{
   width: 100%;
   max-height: 400px;  
 }
 
-
-.section-hl{
+h3{
   position: absolute;
-  top: 5%;
+  top: 3%;
   left: 3%;
   text-align: center;
   color: #FFF;
   font-weight: bold;
-  background-color: #0a2f88;
+  background-color: $main-blue;
   padding: 5px 15px;
 }
 
-
-.bottom-caption{	
+.title{	
 	width:100%;
-  padding:10px 15px;	  
+  padding:10px 15px;
+  color: $black-2;
 }
 
-.bottom-caption h2{    
+.title h2{    
   line-height: 0.03em;
+  color: $black-1;
+}
+.author{
+  color: $black-2;
 }
 
 .separator {
-    background-color: #fbdd01;
+    background-color: $main-red;
     width: 1.5em;
     height: 0.8em;
     margin-top: 5px;
     margin-bottom: 5px;
 }
-
 </style>
