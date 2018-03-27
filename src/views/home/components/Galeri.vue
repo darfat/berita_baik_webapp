dd3f60fb8952c3f0f960d2a70137d168d16da7d8
kashmirx <kashmirx@gmail.com>
2018-03-28 00:37:29 +0700

    add icon, title

--- src/views/home/components/Galeri.vue
+++ src/views/home/components/Galeri.vue
@@ -1,12 +1,26 @@
 <template>  
   <div style="height: 500px; background-color: #323232"   v-loading="loading.galleries" >    
     <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop" >
-      <swiper-slide :class="'slide-'+index" v-bind:style="{ backgroundImage: 'url(' + g.main_image + ')' }"  v-for="(g,index) in galleries" :key="g.id"></swiper-slide>
+      <swiper-slide :class="'slide-'+index" v-bind:style="{ backgroundImage: 'url(' + g.main_image + ')' }"  v-for="(g,index) in galleries" :key="g.id">
+        <div class="icon">
+          <svg-icon icon-class="camera" class="camera"></svg-icon>
+          <span>{{index}}</span>
+        </div>
+        <div class="title">
+          <h2><a>Anyone can steal your idea, but no one can steal your execution.</a></h2>
+          <p>Nadiem Makarim | Start-up Founder -  CEO Gojek</p>
+        </div>
+      </swiper-slide>
       <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
       <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
     </swiper>    
     <swiper :options="swiperOptionThumbs" class="gallery-thumbs" ref="swiperThumbs">
-      <swiper-slide :class="'slide-'+idx" v-bind:style="{ backgroundImage: 'url(' + gt.main_image + ')' }"  v-for="(gt,idx) in galleries" :key="gt.id"></swiper-slide>
+      <swiper-slide :class="'slide-'+idx" v-bind:style="{ backgroundImage: 'url(' + gt.main_image + ')' }"  v-for="(gt,idx) in galleries" :key="gt.id">
+        <div class="icon">
+          <svg-icon icon-class="camera" class="camera"></svg-icon>
+          <span>{{idx}}</span>
+        </div>
+      </swiper-slide>  
     </swiper>
   </div>
 </template>
@@ -81,7 +95,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "src/styles/variables.scss";
   .swiper-container {
     background-color: #323232;
     height: 300px;
@@ -109,11 +124,49 @@ export default {
   .gallery-top {
     height: 80%!important;
     width: 100%;
+    .icon{
+      padding: 20px;
+      color: $main-blue;      
+      .svg-icon{
+        font-size: 30px;
+      }
+      span{
+        font-weight: 900;
+        font-size: 24px;
+      }
+    }
+    .title{
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      background-color: rgba($color: #000000, $alpha: 0.3);
+      color: $bb-white;
+      padding: 10px 20px;
+      font-size: 0.8em;
+      h2{
+        margin: 0;
+        
+      }
+      p{
+        margin: 0;
+      }
+    }
   }
   .gallery-thumbs {
     height: 20%!important;
     box-sizing: border-box;
     padding: 10px 0;
+    .icon{
+      padding: 0 10px;
+      color: $main-blue;      
+      .svg-icon{
+        font-size: 20px;
+      }
+      span{
+        font-weight: 900;
+        font-size: 18px;
+      }
+    }
   }
   .gallery-thumbs .swiper-slide {
     width: 25%;
