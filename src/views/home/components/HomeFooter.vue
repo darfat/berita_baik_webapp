78be195f32ed4a6acc4e22c71984b7aadfa706f4
kashmirx <kashmirx@gmail.com>
2018-03-27 22:34:34 +0700

    css style

--- src/views/home/components/HomeFooter.vue
+++ src/views/home/components/HomeFooter.vue
@@ -1,17 +1,17 @@
 <template>
     <el-footer class="footer-wrapper" height="auto">
         <div class="footer-wrapper-s1">
-          <el-row type="flex" class="container footer-icons">
-            <el-col :span="24">              
-              <a><v-icon name="facebook" base-class="icon-20"></v-icon></a>
-              <a><v-icon name="twitter" base-class="icon-20"></v-icon></a>
-              <a><v-icon name="instagram" base-class="icon-20"></v-icon></a>
-              <a href=""><v-icon name="play" base-class="icon-20"></v-icon></a>
+          <el-row type="flex" class="container">
+            <el-col :span="24" class="footer-icons">              
+              <a href="https://www.facebook.com/beritabaik.id/" target="_blank"><fa-icon name="facebook" scale="1" class="icon" ></fa-icon></a>
+              <a href="https://twitter.com/beritabaik_id" target="_blank"><fa-icon name="twitter" scale="1" class="icon" ></fa-icon></a>
+              <a href="https://www.instagram.com/beritabaik.id/" target="_blank"><fa-icon name="instagram" scale="1" class="icon" ></fa-icon></a>
+              <a href="" target="_blank"><fa-icon name="youtube-square" scale="1" class="icon" ></fa-icon></a>
             </el-col>
           </el-row>
-          <el-row type="flex" class="container footer-nav">
-            <el-col :span="24">
-            <a href="">Home</a>
+          <el-row type="flex" class="container">
+            <el-col :span="24" class="footer-nav">
+            <a href="">Beranda</a>
             <a href="">Tentang Kami</a>
             <a href="">Kontak</a>
             <a href="">Aturan</a>
@@ -24,10 +24,13 @@
         <div class="footer-wrapper-s2">
           <el-row type="flex" class="container footer-copy">
             <el-col :span="12" >
-            Copyright &copy; 2017 PT Warta Milenial Indonesia | <a>Term of Use</a> | <a>Privacy</a>
+            Copyright &copy; {{new Date().getFullYear()}} PT Warta Milenial Indonesia&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a>Term of Use</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a>Privacy</a>
             </el-col>
             <el-col :span="12" style="text-align: right">
-              <a>Sitemap</a> | <a>Contact Us</a>
+              <a>Sitemap</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+              <a>Contact Us</a>
             </el-col>
           </el-row>
           </div>
@@ -37,25 +40,7 @@
 
 <script>
 export default {
-  data() {
-    return {
-      fshare: [
-        { id: 1, name: 'facebook', icon: 'facebook', url: '' },
-        { id: 2, name: 'twitter', icon: 'twitter', url: '' },
-        { id: 3, name: 'instagram', icon: 'instagram', url: '' },
-        { id: 4, name: 'youtube', icon: 'play', url: '' }
-      ],
-      fnav: [
-        { id: 1, name: 'Home', url: '' },
-        { id: 2, name: 'Tentang Kami', url: '' },
-        { id: 3, name: 'Kontak', url: '' },
-        { id: 4, name: 'Aturan', url: '' },
-        { id: 5, name: 'Privasi', url: '' },
-        { id: 6, name: 'Cara Menulis di Berita Baik', url: '' },
-        { id: 7, name: 'Pedoman', url: '' }
-      ]
-    }
-  }
+  data() {}
 }
 
 </script>
@@ -76,25 +61,47 @@ export default {
   }
 }
 
-.footer-wrapper{
-  &-s1{
-    background-color: #333;
-    padding: 20px 0;
-    color: #FEFEFE;
-    font-weight: 700;
-
-    a{
-      padding: 0 20px;
+.footer{
+  &-wrapper{
+    &-s1{
+      background-color: #333;
+      padding: 30px 0 20px 0;      
+      a {
+        color: #FEFEFE;  
+      }
+      a:hover{
+        color: #CCC;
+      }
+    }
+    &-s2{
+      background-color: #1A1A1A;
+      padding: 16px 0;
+      color: #666;
+      font-size: 0.6em;
+      a:hover{
+        color: #ccc;
+      }
     }
   }
-  &-s2{
-    background-color: #1A1A1A;
-    padding: 10px 0;
-    color: #666;
-    font-size: 0.8em;
-    a:hover{
-      color: #ccc;
+  &-icons{
+    a{
+      padding: 0 6px;
     }
+    .icon{
+      width: 34px;
+      height: 34px;
+      border-radius: 50%;
+      padding: 6px;
+      border: 2px solid $bb-white-menu;
+    }
+  }
+  &-nav{
+    font-size: 0.6em;
+    text-transform: uppercase;
+    font-weight: 700;
+    a{
+      padding: 0 10px;
+    }    
   }
 }
 
