c417d483a12ebdca869a100070ca428ff50e0682
kashmirx <kashmirx@gmail.com>
2018-04-12 01:00:11 +0700

    box login style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,7 @@
             <a href="#/home">
               <img :src="img_b_logo" alt="beritabaik.id">
             </a>          
-        </div>
+        </div>        
         <div class="form">
             <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
                 class="card-box login-form">
@@ -20,13 +20,13 @@
                     <svg-icon icon-class="password"></svg-icon>
                   </span>
                   <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
-                    placeholder="password"></el-input>
+                    placeholder="Password"></el-input>
                     <span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>
                 </el-form-item>
                 <el-row :gutter="20">
                     <el-col :span="12">
-                        <div> <a> Lupa Sandi ? </a></div>
-                        <div>   <el-checkbox v-model="checked">Ingat Saya</el-checkbox>  </div>
+                        <div><a> Lupa Sandi ? </a></div>
+                        <div><el-checkbox v-model="checked">Ingat Saya</el-checkbox></div>
                     </el-col>
                     <el-col :span="12" class="align-right">
                         <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
@@ -67,12 +67,10 @@
 
 <script>
 import { isvalidUsername } from '@/utils/validate'
-import img_b_logo from '@/assets/images/logo_beritabaik_b.png'
-import FBSignInButton from 'vue-facebook-signin-button'
-import GSignInButton from 'vue-google-signin-button'
+import img_b_logo from '@/assets/images/ikon_berita_baik.png'
+
 export default {
   name: 'login',
-  component: { FBSignInButton, GSignInButton },
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!isvalidUsername(value)) {
@@ -212,11 +210,11 @@ $lightest_gray:#eef5f8;
       font-weight: bold;
     }
     .login-form {
-      position: absolute;
-      left: 0;
-      right: 0;
+      // position: absolute;
+      // left: 0;
+      // right: 0;
       // width: 400px;
-      padding: 35px 35px 15px 35px;
+      // padding: 35px 35px 15px 35px;
       // margin: 120px auto;
     }
     .el-form-item {
@@ -234,10 +232,27 @@ $lightest_gray:#eef5f8;
       cursor: pointer;
       user-select:none;
     }
+    .el-checkbox__input.is-checked .el-checkbox__inner, .el-checkbox__input.is-indeterminate .el-checkbox__inner {
+        background-color: $main-blue;
+        border-color: $main-blue;
+    }
+    .el-checkbox__input.is-focus .el-checkbox__inner,
+    .el-checkbox__inner:hover {
+      border-color: $main-blue;
+    }
+    .el-checkbox__input.is-checked+.el-checkbox__label {
+      color: $main-blue;
+    }
     .thirdparty-button{
       position: absolute;
       right: 35px;
       bottom: 28px;
+    }
+    .el-button{
+      background-color: $main-blue;
+      color: #fff;
+      border: none;
+      border-radius : 20px; 
     }
   }
   
