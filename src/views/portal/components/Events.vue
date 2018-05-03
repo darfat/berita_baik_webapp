0b48325bd4f698845630f3542a7f727a1a0fde73
darmawan.fatriananda <darmawan.fatriananda@mandrill.com.my>
2018-05-03 15:23:45 +0700

    events home

--- src/views/portal/components/Events.vue
+++ src/views/portal/components/Events.vue
@@ -5,8 +5,12 @@
     <el-row :gutter="20" class="events-title">
         <el-col :xs="24" :sm="8"  >
             <span class="font-black">ACARA </span> <span class="font-blue" >BULAN INI 
-              <v-icon name="chevron-left" base-class="icon-20 v-align-middle"></v-icon>
-              <v-icon name="chevron-right" base-class="icon-20 v-align-middle"></v-icon>
+              <el-button type="text" class="main-blue" @click="handleMonthChanged(-1)" >
+                <v-icon name="chevron-left" base-class="icon-20 v-align-middle" ></v-icon>
+              </el-button>
+              <el-button type="text"  class="main-blue" @click="handleMonthChanged(1)"  >
+                <v-icon name="chevron-right" base-class="icon-20 v-align-middle" ></v-icon>
+              </el-button>
             </span>
         </el-col>
     </el-row>
@@ -15,72 +19,126 @@
         <el-row >
           <el-col >
             <hr style="width:30px;height:5px;background-color:#EB0029; border: none; padding-top:10px; margin-left: 0; clear:both" />
-            <div class="events-month" >DESEMBER</div>
+            <div class="events-month" >{{periodMonth}}</div>
             <div>Cicero famously orated against his political opponent Lucius Sergius Catilina.</div>
           </el-col>
         </el-row>
       </el-col>
       <el-col :xs="24" :sm="6" class="events-separator">
-         <el-row :gutter="20" >
+         <el-row :gutter="20"  >
           <el-col :span="8" class="align-center" >
-             <div class="event-text-circle "> <span class="events-date"> 01 </span> </div>
+             <div class="event-text-circle" > <span class="events-date" v-if="groupedEvents && groupedEvents[0]" > {{groupedEvents[0].date.split("-")[2]}} </span> </div>
           </el-col>
           <el-col :span="16" >
              <div > 
-               <ul>
-                 <li> Event 1</li>
-                 <li> Event 2</li>
-               </ul>
-                
+               <ul v-if="groupedEvents && groupedEvents[0]" >
+                 <li v-for="(event) in groupedEvents[0].events" :key="event.id">{{event.title}}</li>
+               </ul>                
               </div>
           </el-col>
         </el-row>
       </el-col>
       <el-col :xs="24" :sm="6" class="events-separator">
-        <el-row :gutter="20" >
+         <el-row :gutter="20"  >
           <el-col :span="8" class="align-center" >
-             <div class="event-text-circle "> <span class="events-date"> 03 </span> </div>
+             <div class="event-text-circle "> <span class="events-date" v-if="groupedEvents && groupedEvents[1]"> {{groupedEvents[1].date.split("-")[2]}} </span> </div>
           </el-col>
           <el-col :span="16" >
              <div > 
-               <ul>
-                 <li> Event number 1</li>
-                 <li> Event number 2</li>
-               </ul>
-                
+               <ul v-if="groupedEvents && groupedEvents[1]">
+                 <li v-for="(event) in groupedEvents[1].events" :key="event.id">{{event.title}}</li>
+               </ul>                
               </div>
           </el-col>
         </el-row>
       </el-col>
       <el-col :xs="24" :sm="6" class="events-separator">
-        <el-row :gutter="20" >
+         <el-row :gutter="20"  >
           <el-col :span="8" class="align-center" >
-             <div class="event-text-circle "> <span class="events-date"> 18 </span> </div>
+             <div class="event-text-circle "> <span class="events-date" v-if="groupedEvents && groupedEvents[2]"> {{groupedEvents[2].date.split("-")[2]}} </span> </div>
           </el-col>
-          <el-col :span="16" class="" >
+          <el-col :span="16" >
              <div > 
-               <ul>
-                 <li> Event part 1</li>
-                 <li> Event part 2</li>
-               </ul>
-                
+               <ul v-if="groupedEvents && groupedEvents[2]">
+                 <li v-for="(event) in groupedEvents[2].events" :key="event.id">{{event.title}}</li>
+               </ul>                
               </div>
           </el-col>
         </el-row>
       </el-col>
     </el-row>
-    
   </div>
 </template>
 
 <script>
-
+import { getEventsByPeriod } from '@/api/event'
+import moment from 'moment'
 export default {
   name: 'Events',
   data() {
-    return {}
+    return {
+      period: null,
+      periodMonth: '',
+      aDate: null,
+      loading: {
+        events: false,
+        premiumEvents: false
+      },
+      events: [],
+      groupedEvents: []
+    }
   },
   created() {
+    this.init()
+  },
+  mounted() {
+    this.initMounted()
+  },
+  methods: {
+    init() {
+      this.aDate = new Date()
+      this.period = moment(this.aDate).format('YYYYMM')
+      this.periodMonth = moment(this.aDate).format('MMMM')
+    },
+    initMounted() {
+      this.getEventsByPeriod(this.period)
+    },
+    handleMonthChanged(inc) {
+      this.aDate = moment(this.aDate).add(inc, 'month')
+      this.period = this.aDate.format('YYYYMM')
+      this.periodMonth = this.aDate.format('MMMM')
+      this.getEventsByPeriod(this.period)
+    },
+    getEventsByPeriod(period) {
+      this.loading.events = true
+      this.groupedEvents = []
+      getEventsByPeriod({
+        period
+      }).then(response => {
+        if (response && response.data) {
+          const data = response.data
+          // this gives an object with dates as keys
+          const groups = data.reduce((groups, event) => {
+            const date = event.date.split('T')[0]
+            if (!groups[date]) {
+              groups[date] = []
+            }
+            groups[date].push(event)
+            return groups
+          }, {})
+
+          // Edit: to add it in the array format instead
+          this.groupedEvents = Object.keys(groups).map((date) => {
+            return {
+              date,
+              events: groups[date]
+            }
+          })
+          // console.log(this.groupedEvents)
+        }
+        this.loading.events = false
+      })
+    }
   }
 }
 </script>
