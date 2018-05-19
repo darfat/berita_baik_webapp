2f3a9596a24620a10f73aa27701d183a2c9eb0f0
kashmirX <kashmirx@gmail.com>
2018-05-19 18:52:28 +0700

    new layout

--- src/views/home/components/HeadlineSlider.vue
+++ src/views/home/components/HeadlineSlider.vue
@@ -3,24 +3,29 @@
   <el-carousel v-loading="loading.headlines" :interval=9000 >
     <el-carousel-item v-for="item in headlines" :key="item.id">
       <div class="item-wrapper-hl">
-        <router-link :to="{ name: 'article-detail-route', params: { 'editorialSlug':item.editorial.slug, 'slug': item.article.slug,  'articleID': item.article.id} }">
-          <img :src="item.article.main_image" class="image-hl">
-          <h3>{{item.editorial.name}}</h3>
-        </router-link>
-      </div>
-      <div class="share">
-        <bb-love :articleID="item.article.id" :type="'article'" ></bb-love>        
-        <share-pop :article="item.article"></share-pop>
-      </div>
-      <div class="text">      
-        <router-link :to="{ name: 'article-detail-route', params: { 'editorialSlug':item.editorial.slug, 'slug': item.article.slug,  'articleID': item.article.id} }">
-        <h2 class="headline" >{{item.article.title}}</h2>
-        </router-link>
-        <p v-html="item.article.teaser" class="sub-headline" ></p>
-        <p class="red-line"></p>
-        <p class="author">{{ item.article.reporter_name}} | <timeago :since="item.article.publish_date"></timeago>
-        </p>
-      </div>
+        <el-row>
+          <el-col :xs="14" :sm="14">
+            <router-link :to="{ name: 'article-detail-route', params: { 'editorialSlug':item.editorial.slug, 'slug': item.article.slug,  'articleID': item.article.id} }">
+              <img :src="item.article.main_image" class="image-hl">
+              <h3>{{item.editorial.name}}</h3>
+            </router-link>
+          </el-col>
+          <el-col :xs="10" :sm="10">            
+            <div class="info">      
+              <router-link :to="{ name: 'article-detail-route', params: { 'editorialSlug':item.editorial.slug, 'slug': item.article.slug,  'articleID': item.article.id} }">
+              <h2 class="headline" >{{subString(item.article.title, 60)}}</h2>
+              </router-link>
+              <p v-html="subString(item.article.teaser, 160)" class="sub-headline" ></p>
+              <p class="red-line"></p>
+              <p class="author">{{ item.article.reporter_name}} | <timeago :since="item.article.publish_date"></timeago></p>
+              <div class="share">
+                <bb-love :articleID="item.article.id" :type="'article'" ></bb-love>        
+                <share-pop :article="item.article"></share-pop>
+              </div>
+            </div>
+          </el-col>
+        </el-row>        
+      </div>      
     </el-carousel-item>
   </el-carousel>
 </div>
@@ -62,6 +67,13 @@ export default {
           this.headlines = response.data
         }
       })
+    },
+    subString(str, len) {
+      if (str.length < len) {
+        return str
+      } else {
+        return str.substring(0, (len - 3)) + '...'
+      }
     }
   }
 }
