74c5754c901c773fb25b697e9fddf8d994bad489
kashmirx <kashmirx@gmail.com>
2018-05-21 05:12:33 +0700

    profile user page -INIT

---
+++ src/views/public/profile/index.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="profile-wrapper">
+    <el-row :gutter="20" class="profile-header">
+      <div class="container">
+        <div class="img-cover"></div>
+        <div class="img-profile"></div>
+        <div class="name"></div>
+        <div class="edit-btn"></div>
+        <div class="pos"></div>
+        <div class="following"></div>
+        <div class="menu"></div>
+      </div>
+    </el-row>
+    <el-row :gutter="20" class="profile-content">
+      <div class="container">
+      STORY
+      KOMENTAR
+      MENYUKAI
+      > BLANK
+      -icon-buku
+      Belum ada story
+      User ini belum membuat ceritamu sendiri
+      <button>BUAT CERITA</button>
+      </div>
+    </el-row>
+    <el-row :gutter="20" class="profile-other-content">
+      <div class="container">
+      -berita terbaru
+      -berita terpolular
+      -event
+      </div>
+    </el-row>    
+</div>
+</template>
+<script>
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "src/styles/profile.scss";
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 20px;
+  }
+  &:firts-child {
+    // margin-top: 20px;
+  }
+}
+.el-col {
+    border-radius: 0px;
+}
+</style>
