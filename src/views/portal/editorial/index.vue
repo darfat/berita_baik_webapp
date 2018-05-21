f3d6289b586209e414ed46a4384b409a3abb3d1a
kashmirx <kashmirx@gmail.com>
2018-05-21 23:51:13 +0700

    teaser 0.9em

--- src/views/portal/editorial/index.vue
+++ src/views/portal/editorial/index.vue
@@ -13,9 +13,9 @@
             <el-col :xs="24" :sm="10">
               <div class="info">
                 <router-link :to="{ name: 'article-detail-route', params: { 'editorialSlug':latestNews.editorial.slug, 'slug': latestNews.slug,  'articleID': latestNews.id} }">
-                  <h1 class="headline" v-html="latestNews.title"></h1>
-                </router-link>                         
-                <div v-html="latestNews.teaser" class="teaser"></div>
+                  <h1 class="headline" v-html="subString(latestNews.title,50)"></h1>
+                </router-link>
+                <div v-html="subString(latestNews.teaser,250)" class="teaser"></div>                
                 <p class="red-line"></p>
                 <div class="author">
                   {{ latestNews.reporter_name }} | <timeago :auto-update="60" :since="latestNews.publish_date"></timeago>
@@ -135,8 +135,14 @@ export default {
           }
         })
       }
+    },
+    subString(str, len) {
+      if (str.length < len) {
+        return str
+      } else {
+        return str.substring(0, (len - 3)) + '&hellip;'
+      }
     }
-
   }
 }
 </script>
