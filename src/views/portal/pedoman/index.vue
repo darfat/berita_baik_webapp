b8a65864a4c95b89281dac16ab36c02bda94e7cd
kashmirx <kashmirx@gmail.com>
2018-05-30 02:17:00 +0700

    add singe page - pedoman

---
+++ src/views/portal/pedoman/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pedoman-wrapper">
+    <div class="container">
+      <h1 class="headline">Pedoman</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim vel diam eu ultrices. Nulla id neque quis nunc mattis tempus ultricies id purus. Nulla maximus ex a congue pellentesque. Praesent vel risus et erat imperdiet sagittis eget at metus. Suspendisse dapibus faucibus sapien vel porta. Donec pulvinar porta erat, id faucibus risus aliquet ac. Ut vitae viverra dui. Maecenas nec maximus quam. Vivamus mollis ultrices est ut blandit. Donec eu vehicula nunc.</p>
+      <p>Vivamus convallis viverra lobortis. Donec pharetra sapien nec aliquet vulputate. Vivamus fermentum eu lorem a ornare. Sed sagittis erat dui, sit amet malesuada arcu tristique quis. Sed mattis pharetra nisi id euismod. In diam nunc, ultricies sed justo in, facilisis congue nulla. Nulla ornare elit ut malesuada lacinia. Proin fermentum malesuada ex, ut vehicula ante volutpat sed. Pellentesque volutpat semper urna, hendrerit venenatis velit ornare in. Suspendisse posuere mauris sed leo commodo, eu aliquam augue consectetur. Pellentesque sed urna vitae lacus congue malesuada. Aenean eget risus a ex convallis tempus nec eget massa.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pedoman',
+  components: {},
+  data() {
+    return {
+      title: ''
+    }
+  },
+  created() {}
+}
+
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.pedoman-wrapper{  
+  margin: 20px auto;
+  width: 100%;
+  height: auto;
+  background-color: #FFF;
+  padding: 0;
+}
+</style>
