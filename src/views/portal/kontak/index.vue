d15abc37fefed66477f424b7f52b8656e1c525bf
kashmirX <kashmirx@gmail.com>
2018-06-03 01:14:31 +0700

    init page

---
+++ src/views/portal/kontak/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pedoman-wrapper">
+    <div class="container">
+      <h1 class="headline">Kontak</h1>      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Kontak',
+  components: {},
+  data() {
+    return {
+      title: ''
+    }
+  },
+  created() {}
+}
+
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.pedoman-wrapper{  
+  margin: 20px auto;
+  width: 100%;
+  height: auto;
+  background-color: #FFF;
+  padding: 0;
+}
+</style>
